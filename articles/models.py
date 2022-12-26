5d7351d060cd02b1844b0676c278f56163a8e18f
thunder-design <aandha8342@gmail.com>
2022-12-26 17:20:39 -0500

    articles app added

--- articles/models.py
+++ articles/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
   title = models.CharField(max_length=128)
+  slug = models.SlugField(max_length=128)
   body = models.TextField()
   date = models.DateTimeField(auto_now_add=True)
   #thumbnail
   #author
   #word count / char count
+
+  def __str__(self):
+    return self.title
+
+  def snippet(self):
+    return self.body[:50] + "..."
