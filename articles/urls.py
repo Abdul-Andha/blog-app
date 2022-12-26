c7e1e2c7324d53d41ac1d952bbad34fba993da7d
thunder-design <aandha8342@gmail.com>
2022-12-26 18:06:38 -0500

    added named urls

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
   path('', views.article_list, name='list'), #home page of the articles app
   path('<slug:slug>', views.article_detail, name="detail") #specific article page
