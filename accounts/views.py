4b3992816e4653355037b63db902e993ee1bf9ca
thunder-design <aandha8342@gmail.com>
2022-12-26 22:55:15 -0500

    added signup auth

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(req):
-  form = UserCreationForm()
+  if req.method == 'POST':
+    form = UserCreationForm(req.POST) #filled out form to validate and save  
+    if form.is_valid():
+      form.save()
+      #log in
+      return redirect('articles:list')  
+  else:
+    form = UserCreationForm() #blank form to render to the user
+  
   return render(req, 'accounts/signup.html', {'form': form})
