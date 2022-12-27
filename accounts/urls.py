5d20b06990f72a1ac68385c2584085d21c22f0bf
thunder-design <aandha8342@gmail.com>
2022-12-27 13:07:09 -0500

    log in functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-  path('signup/', views.signup_view, name='signup'), #home page of the accounts app
+  path('signup/', views.signup_view, name='signup'), #signup page of the accounts app
+  path('login/', views.login_view, name='login'), #login page of the accounts app
+  path('logout/', views.logout_view, name='logout') 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_view(req):
   if req.method == 'POST':
     form = UserCreationForm(req.POST) #filled out form to validate and save  
     if form.is_valid():
-      form.save()
-      #log in
+      user = form.save()
+      login(req, user)
       return redirect('articles:list')  
   else:
     form = UserCreationForm() #blank form to render to the user
   
   return render(req, 'accounts/signup.html', {'form': form})
+
+def login_view(req):
+  if req.method == 'POST':
+    form = AuthenticationForm(data=req.POST)
+    if form.is_valid(): #log in the user
+      user = form.get_user()
+      login(req, user)
+      return redirect('articles:list')
+  else:
+    form = AuthenticationForm() 
+  
+  return render(req, 'accounts/login.html', {'form': form})
